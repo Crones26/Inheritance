10d338185c8730471f93f4feea714990c8c09f0c
Devya_yi23@top-academy.ru <Devya_yi23@top-academy.ru>
2024-07-23 15:24:19 +0500

    Circle Triangle DONE

--- AbstractGeometryHARD/main.cpp
+++ AbstractGeometryHARD/main.cpp
@@ -1,18 +1,12 @@
 ﻿#include <Windows.h>
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 namespace Geometry
 {
-    //enum (Enumeration - Перечисление) - это набор именованных констрант типа 'int'
     enum Color
-    {	//Здесь используются 8-битные значения, которые могут интерпретироваться по-разному(что не есть хорошо)
-        //CONSOLE_RED = 0xCC,	//старшая 'C' - цвет фона, младшая 'C' - цвет текста.
-        //CONSOLE_GREEN = 0xAA,
-        //CONSOLE_BLUE = 0x99,
-        //CONSOLE_DEFAULT = 0x07
-
-        // Здесь используются 32-битные значения(0xAARRGGBB)
+    {
         CONSOLE_RED = 0x000000FF,
         CONSOLE_GREEN = 0x0000FF00,
         CONSOLE_BLUE = 0x00FF0000,
@@ -33,19 +27,18 @@ namespace Geometry
             case CONSOLE_GREEN: return 0x00FF00; break;
             case CONSOLE_YELLOW: return 0x00FFFF; break;
             }
-            return 0x000000; 
+            return 0x000000;
         }
     public:
         virtual double get_area() const = 0;
         virtual double get_perimeter()const = 0;
-        virtual void draw(int offsetX, int offsetY)const = 0;
+        virtual void draw(HDC hdc, int offsetX, int offsetY)const = 0;
         Shape(Color color) :color(color) {}
         virtual ~Shape() {}
-        virtual void info(int offsetX, int offsetY)const
+        virtual void info()const
         {
             cout << "Площадь фигуры: " << get_area() << endl;
             cout << "Периметр фигуры: " << get_perimeter() << endl;
-            draw(offsetX, offsetY);
         }
     };
 
@@ -74,12 +67,10 @@ namespace Geometry
         {
             return side * 4;
         }
-        void draw(int offsetX, int offsetY)const override
+        void draw(HDC hdc, int offsetX, int offsetY)const override
         {
-            HWND hwnd = FindWindow(NULL, L"Inheritance - Microsoft Visual Studio"); //находит окно с заголовком "Inheritance - Microsoft Visual Studio".
-            HDC hdc = GetDC(hwnd);// получает контекст устройства (HDC) для окна.
             HPEN hPen = CreatePen(PS_SOLID, 5, setRGB(color));// создает сплошное перо заданного цвета и толщины 5 пикселей
-            HBRUSH hBrush = CreateSolidBrush(setRGB(color));//создает кисть заданного цвета.
+            HBRUSH hBrush = CreateSolidBrush(setRGB(color));// создает кисть заданного цвета.
             // выбирает созданные перо и кисть в контекст устройства.
             SelectObject(hdc, hPen);
             SelectObject(hdc, hBrush);
@@ -88,68 +79,19 @@ namespace Geometry
             // удаляет созданные кисть и перо, освобождая ресурсы.
             DeleteObject(hBrush);
             DeleteObject(hPen);
-            ReleaseDC(hwnd, hdc);// освобождает контекст устройства.
         }
-        void info(int offsetX, int offsetY)const override
-        {
-            cout << typeid(*this).name() << endl;
-            cout << "Длина стороны: " << get_side() << endl;
-            Shape::info(offsetX, offsetY);
-        }
-    };
-
-    /*class Square :public Shape
-    {
-        double side;
-    public:
-        Square(double side, Color color) :Shape(color)
-        {
-            set_side(side);
-        }
-        ~Square() {}
-        void set_side(double side)
-        {
-            this->side = side;
-        }
-        double get_side()const
-        {
-            return side;
-        }
-        double get_area()const override
-        {
-            return side * side;
-        }
-        double get_perimeter()const override
-        {
-            return side * 4;
-        }
-        void draw()const override
-        {
-            HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-            SetConsoleTextAttribute(hConsole, color);
-            for (int i = 0; i < side; i++)
-            {
-                for (int i = 0; i < side; i++)
-                {
-                    cout << "* ";
-                }
-                cout << endl;
-            }
-            SetConsoleTextAttribute(hConsole, Color::CONSOLE_DEFAULT);
-        }
-
         void info()const override
         {
             cout << typeid(*this).name() << endl;
             cout << "Длина стороны: " << get_side() << endl;
             Shape::info();
         }
-    };*/
+    };
 
     class Rectangle :public Shape
     {
         double width;    //ширина прямоугольника
-        double height;    //высота прямоугольника
+        double height;   //высота прямоугольника
     public:
         Rectangle(double width, double height, Color color) :Shape(color)
         {
@@ -181,25 +123,25 @@ namespace Geometry
         {
             return (width + height) * 2;
         }
-        void draw(int offsetX, int offsetY)const override
+        void draw(HDC hdc, int offsetX, int offsetY)const override
         {
-            HWND hwnd = FindWindow(NULL, L"Inheritance - Microsoft Visual Studio");
-            HDC hdc = GetDC(hwnd);
-            HPEN hPen = CreatePen(PS_SOLID, 5, setRGB(color));
-            HBRUSH hBrush = CreateSolidBrush(setRGB(color));
+            HPEN hPen = CreatePen(PS_SOLID, 5, setRGB(color));// создает сплошное перо заданного цвета и толщины 5 пикселей
+            HBRUSH hBrush = CreateSolidBrush(setRGB(color));// создает кисть заданного цвета.
+            // выбирает созданные перо и кисть в контекст устройства.
             SelectObject(hdc, hPen);
             SelectObject(hdc, hBrush);
+            // рисует прямоугольник с заданными смещениями offsetX и offsetY.
             ::Rectangle(hdc, offsetX, offsetY, offsetX + width, offsetY + height);
+            // удаляет созданные кисть и перо, освобождая ресурсы.
             DeleteObject(hBrush);
             DeleteObject(hPen);
-            ReleaseDC(hwnd, hdc);
         }
-        void info(int offsetX, int offsetY)const override
+        void info()const override
         {
             cout << typeid(*this).name() << endl;
             cout << "Ширина прямоугольника: " << get_width() << endl;
             cout << "Высота прямоугольника: " << get_height() << endl;
-            Shape::info(offsetX, offsetY);
+            Shape::info();
         }
     };
 
@@ -228,12 +170,10 @@ namespace Geometry
         {
             return 2 * 3.14 * radius;
         }
-        void draw(int offsetX, int offsetY)const override
+        void draw(HDC hdc, int offsetX, int offsetY)const override
         {
-            HWND hwnd = FindWindow(NULL, L"Inheritance - Microsoft Visual Studio"); //находит окно с заголовком "Inheritance - Microsoft Visual Studio".
-            HDC hdc = GetDC(hwnd);// получает контекст устройства (HDC) для окна.
             HPEN hPen = CreatePen(PS_SOLID, 5, setRGB(color));// создает сплошное перо заданного цвета и толщины 5 пикселей
-            HBRUSH hBrush = CreateSolidBrush(setRGB(color));//создает кисть заданного цвета.
+            HBRUSH hBrush = CreateSolidBrush(setRGB(color));// создает кисть заданного цвета.
             // выбирает созданные перо и кисть в контекст устройства.
             SelectObject(hdc, hPen);
             SelectObject(hdc, hBrush);
@@ -242,13 +182,12 @@ namespace Geometry
             // удаляет созданные кисть и перо, освобождая ресурсы.
             DeleteObject(hBrush);
             DeleteObject(hPen);
-            ReleaseDC(hwnd, hdc);// освобождает контекст устройства.
         }
-        void info(int offsetX, int offsetY)const override
+        void info()const override
         {
             cout << typeid(*this).name() << endl;
             cout << "Радиус круга: " << get_radius() << endl;
-            Shape::info(offsetX, offsetY);
+            Shape::info();
         }
     };
 
@@ -288,44 +227,76 @@ namespace Geometry
         {
             return a + b + c;
         }
-        void draw(int offsetX, int offsetY)const override
+        void draw(HDC hdc, int offsetX, int offsetY)const override
         {
-            HWND hwnd = FindWindow(NULL, L"Inheritance - Microsoft Visual Studio"); // ищет окно с заголовком "Inheritance - Microsoft Visual Studio". Если такое окно не найдено, hwnd будет NULL.
-            HDC hdc = GetDC(hwnd); // получает контекст устройства для окна, чтобы можно было рисовать.
-            HPEN hPen = CreatePen(PS_SOLID, 5, setRGB(color));// создает сплошное перо шириной 5 пикселей и заданного цвета (color).
-            HBRUSH hBrush = CreateSolidBrush(setRGB(color)); // создает кисть заданного цвета.
-            //выбирает созданные перо и кисть в контекст устройства.
+            HPEN hPen = CreatePen(PS_SOLID, 5, setRGB(color));// создает сплошное перо заданного цвета и толщины 5 пикселей
+            HBRUSH hBrush = CreateSolidBrush(setRGB(color));// создает кисть заданного цвета.
+            // выбирает созданные перо и кисть в контекст устройства.
             SelectObject(hdc, hPen);
             SelectObject(hdc, hBrush);
-            // задает координаты вершин треугольника с учетом смещений offsetX и offsetY
+            // задает координаты вершин треугольника с учетом смещений offsetX и offsetY.
             POINT points[3] = { {offsetX, offsetY + 100}, {offsetX + 100, offsetY}, {offsetX + 200, offsetY + 100} };
-            ::Polygon(hdc, points, 3);// рисует треугольник по заданным координатам.
-            //удаляет созданные кисть и перо, освобождая ресурсы
+            // рисует треугольник по заданным координатам.
+            ::Polygon(hdc, points, 3);
+            // удаляет созданные кисть и перо, освобождая ресурсы.
             DeleteObject(hBrush);
             DeleteObject(hPen);
-            ReleaseDC(hwnd, hdc); //освобождает контекст устройства, чтобы другие процессы могли использовать его.
         }
-        void info(int offsetX, int offsetY)const override
+        void info()const override
         {
             cout << typeid(*this).name() << endl;
             cout << "Стороны треугольника: " << get_side_a() << ", " << get_side_b() << ", " << get_side_c() << endl;
-            Shape::info(offsetX, offsetY);
+            Shape::info();
         }
     };
+}
+
+void DrawShapesToBitmap(const wchar_t* filename)
+{
+    // Размер изображения
+    int width = 1080;
+    int height = 900;
+
+    // Создание устройства памяти и совместимого контекста устройства
+    HDC hdc = GetDC(NULL);
+    HDC memDC = CreateCompatibleDC(hdc);
+    HBITMAP hBitmap = CreateCompatibleBitmap(hdc, width, height);
+    SelectObject(memDC, hBitmap);
+
+    // Заливка фона белым цветом
+    HBRUSH hBrush = CreateSolidBrush(RGB(255, 255, 255));
+    RECT rect = { 0, 0, width, height };
+    FillRect(memDC, &rect, hBrush);
+    DeleteObject(hBrush);
+
+    // Рисование фигур
+    Geometry::Square square(100, Geometry::Color::CONSOLE_YELLOW);
+    Geometry::Rectangle rectShape(150, 80, Geometry::Color::CONSOLE_BLUE);
+    Geometry::Circle circle(50, Geometry::Color::CONSOLE_GREEN);
+    Geometry::Triangle triangle(100, 100, 100, Geometry::Color::CONSOLE_RED);
+
+    square.draw(memDC, 100, 100);
+    rectShape.draw(memDC, 300, 100);
+    circle.draw(memDC, 500, 100);
+    triangle.draw(memDC, 700, 100);
+
+    // Освобождение ресурсов
+    DeleteObject(hBitmap);
+    DeleteDC(memDC);
+    ReleaseDC(NULL, hdc);
 }
 
 void main()
 {
     setlocale(LC_ALL, "");
-    Geometry::Square square(100, Geometry::Color::CONSOLE_YELLOW);
-    square.info(100, 100);
 
-    Geometry::Rectangle rect(150, 80, Geometry::Color::CONSOLE_BLUE);
-    rect.info(300, 100);
+    // Имя файла для сохранения BMP изображения
+    const wchar_t* filename = L"shapes.bmp";
 
-    Geometry::Circle circle(50, Geometry::Color::CONSOLE_GREEN);
-    circle.info(500, 100);
+    // Рисование фигур в BMP файл
+    DrawShapesToBitmap(filename);
 
-    Geometry::Triangle triangle(100, 100, 100, Geometry::Color::CONSOLE_RED);
-    triangle.info(700, 100);
+    // Открытие BMP файла в Microsoft Paint
+    ShellExecute(NULL, L"open", L"mspaint.exe", filename, NULL, SW_SHOW);
+
 }
